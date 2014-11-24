2d50b0f5f5326f409e60830f744023de4b9fe40d
Josh Ross <josh.ross1234@gmail.com>
2014-11-24 17:45:55 -0500

    Gameboard.java

---
+++ Gameboard.java
@@ -0,0 +1,23 @@
+/**
+ * Write a description of class GameBoard here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GameBoard
+{public GameBoard()
+    {
+        System.out.println("+---+---+---+");
+        System.out.println("|   |   |   | ");
+        
+        System.out.println("+---+---+---+");
+        System.out.println("|   |   |   | ");
+        
+        System.out.println("+---+---+---+");
+        System.out.println("|   |   |   | ");
+        
+        System.out.print("+---+---+---+");
+    }
+      
+
+}
