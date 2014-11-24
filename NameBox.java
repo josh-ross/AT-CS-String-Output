5e633130f3f4b0ccd29a59f9717548151fe5c938
Josh Ross <josh.ross1234@gmail.com>
2014-11-24 17:46:52 -0500

    NameBox.java

---
+++ NameBox.java
@@ -0,0 +1,15 @@
+/**
+ * Write a description of class NameBox here.
+ * 
+ * @author (Josh Ross) 
+ * @version (a version number or a date)
+ */
+public class NameBox
+{
+   public NameBox()
+    {
+        System.out.println("+----+");
+        System.out.println("|Josh|");
+        System.out.println("+----+");
+    }
+}
